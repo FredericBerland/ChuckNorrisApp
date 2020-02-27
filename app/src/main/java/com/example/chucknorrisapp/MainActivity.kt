5551b16e5769dcff88a70da9456bd91d59578106
BERLAND Frédéric <frederic.berland@edu.esiee.com>
2020-02-27 09:31:45 +0100

    new question 1.1

--- app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("list:", list.toString())
+        Log.d("list:", Jokes.list.toString())
+
     }
 }
