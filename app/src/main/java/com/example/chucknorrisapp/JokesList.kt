fbd4ce373ff7edcd959ebe2334fdb825b57e742c
BERLAND Frédéric <frederic.berland@edu.esiee.com>
2020-02-27 09:19:34 +0100

    question 1.1

---
+++ app/src/main/java/com/example/chucknorrisapp/JokesList.kt
@@ -0,0 +1,8 @@
+package com.example.chucknorrisapp
+
+val list = listOf("Old Mother Hubbard went to her cupboard to get her poor doggy a bone. But when she bent over, Chuck Norris took over, 'cause Chuck had a bone of his own.",
+    "everyone on roblox is about to be roundhousekicked by Chuck Norris", "Chuck Norris enjoys hang gliding after dark with a pair of night-vision goggles and a bunch of nooses.",
+    "Chuck Norris once threw a Kenworth into the Pacific Ocean. It became known as Octopus Prime.", "Chuck Norris was the only professional boxer ever who was allowed to perform fatality moves on his defeated opponents.",
+    "Chuck Norris can slice your head off with a baseball bat.", "Chuck Norris was so pissed, he accidently hit something that borke the moon.",
+    "There is no Zuul. There is only Chuck Norris", "Chuck Norris can faithfully reproduce Beethoven's Ninth using a jug, washboard, harmonica, and a pair of spoons.",
+    "Behind every successful man, there is a woman. Behind every dead man, there is Chuck Norris.")
