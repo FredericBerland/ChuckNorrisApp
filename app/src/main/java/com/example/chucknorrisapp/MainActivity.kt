ef5d3246b5eb0f958d264895e570ab2b9419db93
BERLAND Frédéric <frederic.berland@edu.esiee.com>
2020-04-09 15:31:36 +0200

    IV - 1 Screen Rotation implémenter avec succes

--- app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.chucknorrisapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.ProgressBar
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.size
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -16,6 +13,10 @@ import io.reactivex.disposables.CompositeDisposable
 import kotlinx.serialization.UnstableDefault
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.serializer
+import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 
@@ -23,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var aListeOfJokes: List<Joke>
+    private lateinit var aVA : JokesAdapter
     private lateinit var vPB : ProgressBar
     private val cD = CompositeDisposable()
 
@@ -32,11 +35,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewManager = LinearLayoutManager(this)
-
         val viewAdapter = JokesAdapter()
+        aVA = viewAdapter
 
         vPB = findViewById(R.id.main_pb)
-
         recyclerView = findViewById<RecyclerView>(R.id.main_rv).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
@@ -47,36 +49,64 @@ class MainActivity : AppCompatActivity() {
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
-                if( (viewManager as LinearLayoutManager).findLastVisibleItemPosition() == viewAdapter.itemCount-1){
                     add10Joke(viewAdapter)
-                }
             }
         })
     }
 
-
-    @UnstableDefault
-    fun addAJoke( viewAdapter : JokesAdapter){
-        val jS = JokeApiServiceFactory.returnService().giveMeAJoke()
-        val vR = jS
-            .delay(500, TimeUnit.MILLISECONDS)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { vPB.visibility = View.VISIBLE }
-            .doAfterSuccess { vPB.visibility = View.INVISIBLE }
-            .subscribeBy(onError = {Log.d("Error :", "$it")}, onSuccess = { viewAdapter.jokes = viewAdapter.jokes.plus(it) })
-        cD.add(vR)
-    }
-
     @UnstableDefault
     fun add10Joke( viewAdapter : JokesAdapter){
-        val vR = JokeApiServiceFactory.returnService().giveMeAJoke().repeat(10)
-            .delay(500, TimeUnit.MILLISECONDS)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { vPB.visibility = View.VISIBLE }
-            .doAfterTerminate { vPB.visibility = View.INVISIBLE }
-            .subscribeBy(onError = {Log.d("Error :", "$it")}, onNext = { viewAdapter.jokes = viewAdapter.jokes.plus(it) }, onComplete = {  })
-        cD.add(vR)
+        if( (viewManager as LinearLayoutManager).findLastVisibleItemPosition() == viewAdapter.itemCount-1) {
+            val vR = JokeApiServiceFactory.returnService().giveMeAJoke().repeat(10)
+                .delay(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { vPB.visibility = View.VISIBLE }
+                .doAfterTerminate { vPB.visibility = View.INVISIBLE }
+                .subscribeBy(
+                    onError = { Log.d("Error :", "$it") },
+                    onNext = { viewAdapter.jokes = viewAdapter.jokes.plus(it) },
+                    onComplete = { aListeOfJokes = viewAdapter.jokes })
+            cD.add(vR)
+
+        }
+    }
+
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle){
+        super.onSaveInstanceState(savedInstanceState)
+        var i = 0
+        savedInstanceState.putInt("Size", aListeOfJokes.size)
+        aListeOfJokes.forEach {
+            savedInstanceState.putString("Joke$i", Json(JsonConfiguration.Stable).stringify(Joke.serializer(), it))
+            i++
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        try {
+            var vList = emptyList<Joke>()
+            val n = savedInstanceState.getInt("Size")
+            lateinit var vS : String
+
+            for(i in 0 until n-1){
+                try {
+                    //recuperation de la string sérialiser
+                    vS = savedInstanceState.getString("Joke$i")!!
+                    //recuperation de la joke depuis le string sérialiser
+                    vList = vList.plus(Json(JsonConfiguration.Stable).parse(Joke.serializer(), vS))
+                }
+                catch (e : Exception){
+                    Log.d("ERROR2", e.toString())
+                }
+            }
+
+            //ajout des jokes + notify
+            aVA.jokes = aVA.jokes.plus(vList)
+        }
+        catch (e : Exception){
+            Log.d("ERROR", e.toString())
+        }
     }
 }
