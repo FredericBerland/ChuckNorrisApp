1e85b740ae088689a1b93ba14abcb4f760182571
BERLAND Frédéric <frederic.berland@edu.esiee.com>
2020-04-15 15:44:57 +0200

    IV - 5 SwipRefresh implémenter avec succes Version Finale

--- app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.chucknorrisapp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
@@ -17,7 +18,6 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
-import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 
@@ -26,10 +26,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var aVA : JokesAdapter
-    private lateinit var vPB : ProgressBar
     private val cD = CompositeDisposable()
     private lateinit var aFav : SharedPreferences
+    private lateinit var swip: SwipeRefreshLayout
 
+    @SuppressLint("ResourceAsColor")
     @UnstableDefault
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +45,10 @@ class MainActivity : AppCompatActivity() {
         val viewAdapter = JokesAdapter(vFavList)
         aVA = viewAdapter
 
-        vPB = findViewById(R.id.main_pb)
+        swip = findViewById(R.id.main_swip)
+        swip.setProgressBackgroundColorSchemeResource(R.color.blanc)
+        swip.setColorSchemeResources(R.color.rose)
+
         recyclerView = findViewById<RecyclerView>(R.id.main_rv).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
@@ -52,13 +56,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if ((viewManager as LinearLayoutManager).findLastVisibleItemPosition() == viewAdapter.itemCount - 1 || (viewManager as LinearLayoutManager).findLastVisibleItemPosition() == -1)
                     add10Joke(viewAdapter)
+                Log.d("SCROLL", "SCROLL")
             }
         })
 
-        val callback = JokeTouchHelper( {m-> viewAdapter.deleteJoke(m)}, {m, n-> viewAdapter.moveInList(m, n)})
+        val callback = JokeTouchHelper({ m -> viewAdapter.deleteJoke(m) },
+            { m, n -> viewAdapter.moveInList(m, n) })
         callback.attachToRecyclerView(recyclerView)
 
         add10Joke(viewAdapter)
@@ -66,19 +73,17 @@ class MainActivity : AppCompatActivity() {
 
     @UnstableDefault
     fun add10Joke( viewAdapter : JokesAdapter){
-        if( (viewManager as LinearLayoutManager).findLastVisibleItemPosition() == viewAdapter.itemCount-1 || (viewManager as LinearLayoutManager).findLastVisibleItemPosition()==-1) {
-            val vR = JokeApiServiceFactory.returnService().giveMeAJoke().repeat(10)
-                .delay(500, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { vPB.visibility = View.VISIBLE }
-                .doAfterTerminate { vPB.visibility = View.INVISIBLE }
-                .subscribeBy(
-                    onError = { Log.d("Error :", "$it") },
-                    onNext = { viewAdapter.jokes = viewAdapter.jokes.plus(it) },
-                    onComplete = { })
-            cD.add(vR)
-        }
+        val vR = JokeApiServiceFactory.returnService().giveMeAJoke().repeat(10)
+            .delay(500, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { swip.isRefreshing = true }
+            .doAfterTerminate { swip.isRefreshing = false }
+            .subscribeBy(
+                onError = { Log.d("Error :", "$it") },
+                onNext = { viewAdapter.jokes = viewAdapter.jokes.plus(it) },
+                onComplete = { })
+        cD.add(vR)
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle){
